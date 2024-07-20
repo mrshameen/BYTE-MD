44f03feb7206c82ebea5389adc6cd014aa59dd90
SL Shanuwa <Shameendurage@gmail.com>
2024-07-20 08:25:55 +0530

    Update SHANUWA-MD

--- config.js
+++ config.js
@@ -19,20 +19,20 @@ global.website = process.env.GURL || "https://whatsapp.com/channel/0029VaNRcHSJP
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://raw.githubusercontent.com/HyHamza/HyHamza/main/Images/logo.jpg";
 global.devs = "923072380380";
 global.sudo = process.env.SUDO || "923072380380";
-global.owner = process.env.OWNER_NUMBER || "923072380380";
+global.owner = process.env.OWNER_NUMBER || "94724389699";
 global.style = process.env.STYLE || "3";
 global.gdbye = process.env.GOODBYE || "false";
-global.wlcm = process.env.WELCOME || "false";
+global.wlcm = process.env.WELCOME || "ture";
 global.warncount = process.env.WARN_COUNT || 3;
 global.disablepm = process.env.DISABLE_PM || "false";
-global.disablegroup = process.env.DISABLE_GROUPS || "false",
-global.MsgsInLog = process.env.MSGS_IN_LOG || "false";
+global.disablegroup = process.env.DISABLE_GROUPS || "ture",
+global.MsgsInLog = process.env.MSGS_IN_LOG || "ture";
 global.userImages = process.env.USER_IMAGES || "";
 global.waPresence = process.env.WAPRESENCE || "available";
 global.readcmds = process.env.READ_COMMAND || "true";
 global.readmessage = process.env.READ_MESSAGE || "false";
 global.readmessagefrom = process.env.READ_MESSAGE_FROM || "";
-global.read_status = process.env.AUTO_READ_STATUS || "false";
+global.read_status = process.env.AUTO_READ_STATUS || "ture";
 global.save_status = process.env.AUTO_SAVE_STATUS || "false";
 global.save_status_from = process.env.SAVE_STATUS_FROM || "all";
 global.read_status_from = process.env.READ_STATUS_FROM || "all";
@@ -57,7 +57,7 @@ global.SESSION_ID =
 
 
   
-  "Your session ID" //Make sure session id starts with Byte;;;
+  "Byte;;;eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiV0VCMmU2VTN1YVA0WVp0NVZnd0NrLzJCZzk2WU1YTk5QNTljYmtiK0JtTT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiOEVEZW9jUzlQS0tCcGMrSjRnUTJCdUdDc29EQVptQldRVDdWelc4Nnd5Yz0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ5SDNMb0tTL29VWjFHQmZheWZjbDBGMzhGZlg2bG1MNUNZQ2QzSGkwcUVRPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJaY1cxZGp4ckVLaVNleEk3YlVrQWRGWE9YOE9UTkZMZ0lsTXpQQVM0TVJzPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IitOT1hrYXI5a0dyMFc1WXY1VXBQNXJvTnRpOEFRL3Ayb1ZZMEw4WU92bTg9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJ3d1VWVVN5NktqMVVNelZsN3JVRkZYSlZ3UGtrekZiL0NXL0dhRWc4MDQ9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiS0RWazZjOWxVRndtV3VJK1YxTXcrcTBxandKTlMvQ0kzcXI3Q0k3RWJXND0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoicktSaDZxY0Z5Q1E4RGxKb2xJd1JDNVo4UDBDM24rVU1FMXdQMUl1elZ3OD0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlpSQjA0ZEJVdTZLclBaOWUwTXRjb1MzOEw4L0k0bVpjMXpWaU5QMXJTdzlVbFlQOVQvTXdOTHp3enZjNEdWeFFmcmlMR1FIc1YvU1h6VlFkbVRRV0RRPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MjM5LCJhZHZTZWNyZXRLZXkiOiJxT1FNZnNGakpZUnpqRWtKc3Bqc3o3TnA3TzVZMTVzWmxHcFJjamIxWERrPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzIsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMiwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJtWXptNWZjdlF5S0J6UUdkWFFNQWVBIiwicGhvbmVJZCI6ImJhZTRiOThhLTExYmUtNDI3Ni04MDc3LWFlYzlmZTMzOWEzOSIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJyOTJZNk1Mc0s5WTQydnh4aU5XbDRhRVZGNXM9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoibGhnZTBCSHBEUHlHZmtYODQyVGkvQ3ZYdXpnPSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6IjdIRVdTWTY0IiwibWUiOnsiaWQiOiI5NDcyNDM4OTY5OToxMkBzLndoYXRzYXBwLm5ldCJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDT2l6NDY0R0VPdkw3TFFHR0FRZ0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoiYzRqS0dnRDZPbURFTWtHNXZwN2RIbGVwR3Y4UjAxOGhjNTAyeE0vaHowZz0iLCJhY2NvdW50U2lnbmF0dXJlIjoiZ2lMdGF6aCtaS3dkelJobjdPUE1oMDBRcE5jV3FxeWJabGJyQW5hbFZiNHdVczdNdm9HTnFaaEVCSytqczhmNW5UTWhSK2YrNzVESXdna3E5eElTREE9PSIsImRldmljZVNpZ25hdHVyZSI6IkhyK2p0M20vL25LZWF2K0RpaFp3LzR4UmJncXlxbVNsWDA1TTB0ajd2dXZwU0xqQzViZzRJbTZjWnQ0OEYxWVNmUkZzL1A3eEdXRjI1R244TUMwNkRRPT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiOTQ3MjQzODk2OTk6MTJAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCWE9JeWhvQStqcGd4REpCdWI2ZTNSNVhxUnIvRWROZklYT2ROc1RQNGM5SSJ9fV0sInBsYXRmb3JtIjoiYW5kcm9pZCIsImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcCI6MTcyMTQ0MzgzMSwibXlBcHBTdGF0ZUtleUlkIjoiQUFBQUFEUmkifQ==" //Make sure session id starts with Byte;;;
 
 
 
